ff3fb646f466145e7c9ad9a68c7ef1ebc0d340d2
feyifoluwa <abioyefeyifoluwa@gmail.com>
2020-11-12 18:51:49 +0100

    db

--- config/database.php
+++ config/database.php
@@ -45,13 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'url' => 'DATABASE_URL',
+            'host' => 'DB_HOST', 'localhost',
+            'port' => 'DB_PORT', '3306',
+            'database' => 'DB_DATABASE', 'forge',
+            'username' => 'DB_USERNAME', 'forge',
+            'password' => 'DB_PASSWORD', '',
+            'unix_socket' => 'DB_SOCKET', '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => 'MYSQL_ATTR_SSL_CA',
             ]) : [],
         ],
 
