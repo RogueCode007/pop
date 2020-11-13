71b4f3d02d796789eb531d03b2f8faa82193b1c0
feyifoluwa <abioyefeyifoluwa@gmail.com>
2020-11-13 07:54:48 +0100

    create seed

---
+++ database/seeders/ItemsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Item::create([
+            'name'=> 'sample1',
+            'price'=> 4000,
+            'description'=> 'random string1',
+            'category'=> 'dress',
+            'image_name'=> 'randomname1'
+        ]);
+        Item::create([
+            'name'=> 'sample2',
+            'price'=> 5000,
+            'description'=> 'random string2',
+            'category'=> 'shoe',
+            'image_name'=> 'randomname2'
+        ]);
+    };
+}
