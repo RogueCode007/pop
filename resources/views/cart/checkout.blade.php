786e09a11b9fa11f7e3bb1b68947d10af36f22a1
feyifoluwa <abioyefeyifoluwa@gmail.com>
2020-11-14 11:30:39 +0100

    edit checkout

--- resources/views/cart/checkout.blade.php
+++ resources/views/cart/checkout.blade.php
@@ -2,14 +2,7 @@
 @section('content')
   
 <div class="container-lg">
-  @if(session()->has('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-      {{ session()->get('success') }}
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">×</span>
-      </button>
-    </div>
-  @endif
+  <p>Thanks for your order!</p>
   <a href="{{route('items.index')}}">Back to shop</a>
 </div>
 @endsection
